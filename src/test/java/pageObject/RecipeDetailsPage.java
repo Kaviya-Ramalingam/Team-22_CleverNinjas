e4e6035a3b325e83f68e73a18b463c67a07660b4
TrioChallengers <enggnishu@gmail.com>
2023-12-16 03:07:00 -0500

    created getRecipeIngrediants method in recipe detail page

--- src/test/java/pageObject/RecipeDetailsPage.java
+++ src/test/java/pageObject/RecipeDetailsPage.java
@@ -1,23 +1,31 @@
 package pageObject;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import Base.BaseTest;
 
-public class RecipeDetailsPage  extends BaseTest{
-	
+public class RecipeDetailsPage extends BaseTest {
 
 	WebDriver driver;
 
-	
+	@FindBy(xpath = "//div[@id='rcpinglist']")
+	public WebElement recipeIngrediants;
+
 	public RecipeDetailsPage(WebDriver driver) {
-	
-		this.driver=driver;
-		
+
+		this.driver = driver;
+
 		PageFactory.initElements(driver, this);
 	}
+
+	public String getRecipeIngrediants() {
+		String txt = recipeIngrediants.getText();
+		System.out.println("Recipe Ingrediants: "+txt);
+		return txt;
+	}
 }
-	
-
-
